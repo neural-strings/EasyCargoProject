57ccc4c9c8570a90f100a74ae3d571d033d2b1b6
nikitakobzev <ni.ki.ta.12111998@gmail.com>
2024-06-11 12:37:01 +0300

    Test JPA

---
+++ src/main/java/org/easyCargoProject/org/EasyCargoProjectApplication.java
@@ -0,0 +1,11 @@
+package org.easyCargoProject.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCargoProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCargoProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/org/easyCargoProject/org/EasyCargoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.easyCargoProject.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCargoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
